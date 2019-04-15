7f111fa440fb3c6cd96d062885f7517f7864a8f6
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-04-15 10:26:00 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Auto_Galery
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(162, 196);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 45);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Licence Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Brand";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Model";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Fuel Type";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Toyota",
+            "Honda",
+            "Opel"});
+            this.comboBox1.Location = new System.Drawing.Point(144, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(144, 93);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Gasoline",
+            "Diesel",
+            "Gasoline && LPG"});
+            this.comboBox3.Location = new System.Drawing.Point(144, 122);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(294, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Back Type";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(294, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Kms";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(294, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Price";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Sedan",
+            "Hatchback"});
+            this.comboBox4.Location = new System.Drawing.Point(394, 70);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 24);
+            this.comboBox4.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(394, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 22);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(394, 129);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 22);
+            this.textBox3.TabIndex = 17;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(294, 196);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 45);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Update (Price)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 258);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(995, 180);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Auto_Galery.Properties.Resources.foto;
+            this.pictureBox1.Location = new System.Drawing.Point(521, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(486, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 442);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Auto_Galery
+{
+    public partial class Form1 : Form
+    {
+        private void listRegistry()
+        {
+            try
+            {
+                connecion.Open();
+                OleDbDataAdapter list = new OleDbDataAdapter("select * from vehicles", connecion);
+                //-----// Sorgumuzun oluşturulmasını sağlıyoruz //-----// 
+                DataSet dsmemory = new DataSet(); //-----// data set ile sonuçları hafızaya saklıyoruz //-----//
+                list.Fill(dsmemory);
+                dataGridView1.DataSource = dsmemory.Tables[0]; //-----// Datagridview in veri kaynağı olan ilk tabloyu aldık, datagridview'e aktardık //-----//
+                connecion.Close();
+            }
+            catch (Exception explenation)
+            {
+
+                MessageBox.Show(explenation.Message);
+                connecion.Close();
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listRegistry();
+        }
+        OleDbConnection connecion = new OleDbConnection("Provider=Microsoft.JET.OleDb.4.0; Data Source=" +
+            Application.StartupPath+"\\Auto_Galery.mdb");
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();   //-----// veriler alt alta yığılmasın diye her seferinde temizliyoruz //-----//
+            string brand = comboBox1.SelectedItem.ToString();
+            if (brand == "Toyota")
+            {
+                string[] model = { "Auris", "Yaris", "Corolla" };
+                comboBox2.Items.AddRange(model);   //-----// Elemanların eklenmesini sağlıyoruz //-----//
+            }
+            if (brand == "Honda")
+            {
+                string[] model = { "Civic", "Accord" };
+                comboBox2.Items.AddRange(model);
+            }
+            if(brand == "Opel")
+            {
+                string[] model = { "Astra", "Vectra", "Corsa" };
+                comboBox2.Items.AddRange(model);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connecion.Open();
+                //------//----------------------------------------------------------//------//
+                OleDbDataAdapter addcommand = new OleDbDataAdapter
+                    ("insert into vehicles (licenceno,brand,model,fuel_type,back_type,kilometer,price) values " +
+                    "('" + textBox1.Text + "','" + comboBox1.SelectedItem.ToString()
+                    + "','" + comboBox2.SelectedItem.ToString() + "','" + comboBox3.SelectedItem.ToString() + "','" + comboBox4.SelectedItem.ToString()
+                    + "','" + textBox2.Text + "','" + textBox3.Text + " ')", connecion);
+                //------//Araçlar tablosuna kayıt ekle (alanar verildi) //------//
+                //------//Valuesten sonra sırasıyla bu alanlara değer veriyoruz //------//
+                DataSet dsmemory = new DataSet();        //------//sorgunun sonuçlarının bellekte geçici olarak depolanması için------------//------//
+                addcommand.Fill(dsmemory);              //------//sonuçların dsmemory'e aktarılmasını sağladık ve access tablosuna işledik.//------//
+                connecion.Close();
+                MessageBox.Show("Vehicle has been added to DataBase");
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                listRegistry();             //------//Kullanıcı anlık olarak verileri görsün diye bu metodu çağırdık//------//
+            }
+            catch (Exception explenation)
+            {
+                MessageBox.Show(explenation.Message);
+                connecion.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connecion.Open();
+                OleDbDataAdapter deletecommand = new OleDbDataAdapter("delete from vehicles where licenceno='"+textBox1.Text+"'",connecion);
+                //------//------------------------------------Ruhsat no'su tb1 ile eşleşenleri sil----------------------------//------//
+                DataSet dsmemory = new DataSet();
+                deletecommand.Fill(dsmemory);
+                connecion.Close();
+                MessageBox.Show("Vehicle has been deleted from DataBase");
+                listRegistry();
+            }
+            catch  (Exception explenation)
+            {
+                MessageBox.Show(explenation.Message);
+                connecion.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connecion.Open();
+                OleDbDataAdapter update = new OleDbDataAdapter("update vehicles set price='" + textBox3.Text + "'where licenceno='" + textBox1.Text + "'", connecion);
+                DataSet dsmemory = new DataSet();
+                update.Fill(dsmemory);
+                connecion.Close();
+                MessageBox.Show("Vehicle's price has been updated");
+                listRegistry();
+
+            }
+            catch (Exception explanation)
+            {
+                MessageBox.Show(explanation.Message);
+                connecion.Close();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selected = dataGridView1.SelectedCells[0].RowIndex;
+            textBox1.Text = dataGridView1.Rows[selected].Cells[0].Value.ToString();
+            comboBox1.Text = dataGridView1.Rows[selected].Cells[1].Value.ToString();
+            comboBox2.Text = dataGridView1.Rows[selected].Cells[2].Value.ToString();
+            comboBox3.Text = dataGridView1.Rows[selected].Cells[3].Value.ToString();
+            comboBox4.Text = dataGridView1.Rows[selected].Cells[4].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[selected].Cells[5].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[selected].Cells[6].Value.ToString();
+        }
+    }
+}
